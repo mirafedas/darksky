d337befdd6704be5341a099f4796cb3552905e33
myr <30750556+myrosvas@users.noreply.github.com>
2018-02-04 17:09:52 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,37 @@
-var darkskyKey = '841e534fb31d0704d2286fa1e12346cf',
-latitude,
-longitude;
+//vars
+var darkskyKey = '841e534fb31d0704d2286fa1e12346cf';
+
+//Getting users position:
+window.onload = function findLocation() {
+    var startPos;
+    var geoSuccess = function(position) {
+      startPos = position;
+      document.getElementById('startLat').innerHTML = startPos.coords.latitude.toFixed(4);
+      document.getElementById('startLon').innerHTML = startPos.coords.longitude.toFixed(4);
+    };
+    var geoError = function(error) {
+        alert('Your position is not defined');
+    };
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  };
+
+//Preparing data to make a request to Darksky.net:
 
 var url = 'https://api.darksky.net/forecast/' + darkskyKey + '/' + latitude + ',' + longitude;
 
-window.onload(getForecast());
+//Reguesting the today weather from Darksky.net:
+
+//Rendering requested data:
+
+//Reguesting the yesterday weather from Darksky.net:
+
+//Rendering requested data:
+
+//Reguesting the tomorrow weather from Darksky.net:
+
+//Rendering requested data:
+
+//window.onload(getForecast());
 // var getForecast = function (url, callback) {
 //     var xhr = new XMLHttpRequest();
 //     xhr.open("GET", url, true);
